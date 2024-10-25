79184b451dff142fcc93c21b8e695438e162c0fc
TheDarkLady <lakshmichaithanya13@gmail.com>
2024-10-25 10:56:27 +0530

    signup to login redirection is completed

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -6,7 +6,7 @@ function LoginForm() {
     <div className='form-container'>
         <form onSubmit={() => navigate('/home')}>
             <label htmlFor="username" name="name" type="text" className='form-label'>Username</label>
-            <input type="text" name="username" id="username"   className='form-input'/>
+            <input type="text" name="username" id="username"   className='form-input' />
             <label htmlFor="password" name="password" type="text" className='form-label'>password</label>
             <input type="text" name="password" id="password"   className='form-input'/>
             <div style={{display: "flex", justifyContent: "center"}}>
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { BrowserRouter } from 'react-router-dom'
+import LoginForm from './LoginForm'
 function SignUp() {
+  const [showLoginForm, setShowLoginForm] = useState(false)
   const navigate = useNavigate()
   const [formData, setFormData] = useState({
     username: '',
@@ -10,27 +12,37 @@ function SignUp() {
   })
   
   function handleChange(e){
-    setFormData({...formData, [e.target.name] : e.target.value})
+    // const {name, value} = e.target
+    setFormData({ ...formData, [e.target.name]: e.target.value })
   }
-  function handleSubmit() {
-    console.log(formData)
-    localStorage.setItem('formData', JSON.stringify(formData))
-    navigate('/home')
+  function handleSubmit(e) {
+    e.preventDefault()
+    const existingData = JSON.parse(localStorage.getItem('formData')) || []
+    const updatedData = [...existingData, formData]
+    console.log(updatedData )
+    localStorage.setItem('formData', JSON.stringify(updatedData))
+    // navigate('/home')
+    alert('Sign up is successfull ! Please Login')
+    setShowLoginForm(true)
   }
   return (
-    <div className='form-container'>
+    <div>
+      {showLoginForm ? (<LoginForm />) : (
+        <div className='form-container'>
         <form onSubmit={handleSubmit}>
             <label htmlFor="username" name="name" type="text" className='form-label' style={{marginBottom: "20px"}}>Username</label>
-            <input type="text" name="username" id="username" required   className='form-input' value={formData.username} onChange={handleChange}/>
+            <input type="text" name="username" id="username"    className='form-input' value={formData.username} onChange={handleChange}/>
             <label htmlFor="email" name="email" type="email" className='form-label'>Email</label>
-            <input type="email" name="email" id="email" required  className='form-input' value={formData.email} onChange={handleChange}/>
+            <input type="email" name="email" id="email"   className='form-input' value={formData.email} onChange={handleChange}/>
             <label htmlFor="password" name="password" type="text" className='form-label'>password</label>
-            <input type="text" name="password" id="password" required  className='form-input' value={formData.password} onChange={handleChange}/>
+            <input type="text" name="password" id="password"   className='form-input' value={formData.password} onChange={handleChange}/>
             <div style={{display: "flex", justifyContent: "center"}}>
-            <button className='form-button'>Sign Up</button>
+            <button  type="submit" className='form-button'>Sign Up</button>
             </div>
         </form>
       
+    </div>
+      )}
     </div>
   )
 }
