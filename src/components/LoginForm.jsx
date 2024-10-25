a2d5e7f18c8c3c7cecaf706c4f41c10cb85553fa
TheDarkLady <lakshmichaithanya13@gmail.com>
2024-10-25 12:07:28 +0530

    login or signup redirecion to home page is completed as expected

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,14 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter, useNavigate } from 'react-router-dom'
 function LoginForm() {
   const navigate = useNavigate()
+  const [loginData, setLoginData] = useState({
+    username: '',
+    password:''
+  })
+  const [errormsg, setErrormsg] = useState('')
+  function handleChange(e){
+    setLoginData({...loginData, [e.target.name]: e.target.value})
+  }
+
+  function handleSubmit(e){
+    e.preventDefault()
+    console.log(loginData)
+    const existingData = JSON.parse(localStorage.getItem('formData')) || []
+    const user = existingData.find(user => user.username === loginData.username && user.password === loginData.password)
+    if(user){
+      navigate('/home')
+    }
+    else{
+      // alert('Details Incorrect')
+      setErrormsg('Details Incorrect')
+    }
+  }
   return (
     <div className='form-container'>
-        <form onSubmit={() => navigate('/home')}>
+        <form onSubmit={handleSubmit}>
             <label htmlFor="username" name="name" type="text" className='form-label'>Username</label>
-            <input type="text" name="username" id="username"   className='form-input' />
+            <input type="text" name="username" id="username"   className='form-input' value={loginData.username} onChange={handleChange}/>
             <label htmlFor="password" name="password" type="text" className='form-label'>password</label>
-            <input type="text" name="password" id="password"   className='form-input'/>
+            <input type="text" name="password" id="password"   className='form-input' value={loginData.password} onChange={handleChange}/>
+            {errormsg && (<p style={{color: '#fff', border: '1px solid red', padding: '5px', borderRadius: '5px', textAlign: 'center'}}>{errormsg}</p>)}
             <div style={{display: "flex", justifyContent: "center"}}>
             <button className='form-button'>Login</button>
             </div>
